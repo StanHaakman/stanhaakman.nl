a0b404b17989e6f6baddf014f0792441ae978f19
stanhaakman <stanhaakman@icloud.com>
2022-03-14 00:36:22 +0100

    Dark mode implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './styles/_App.sass';
 
 function App() {
-  return (
-      <main className="relative flex items-center justify-center h-screen">
+    return (
+      <main className="relative flex items-center justify-center h-screen dark:bg-darkBg transition-all duration-500">
           <h1>Website in progress</h1>
       </main>
-  );
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,40 @@
 import switcher from './../images/switch_bg.svg';
 import logo from './../images/stanLogo.svg';
+import logoDarkMode from './../images/stanLogoDarkMode.svg';
 import moon from './../images/moon.svg'
 import circle from './../images/circle.svg'
+import useDarkMode from "../hook/useDarkMode";
 
-function Navbar() {
+function Navbar(props) {
+    const [colorTheme, setTheme] = useDarkMode();
+
     return (
-      <nav className="nav fixed top-0 flex w-full px-10 py-5 justify-between items-center">
-          <div className="switcher relative justify-center items-center">
+      <nav className="nav fixed top-0 flex w-full px-10 py-5 justify-between items-center z-10">
+          <span onClick={() => setTheme(colorTheme)} className="switcher relative justify-center items-center cursor-pointer h-fit">
               <img src={switcher} alt="" className=""/>
-          </div>
+              <img className="absolute top-1/2 -translate-y-1/2 left-1 dark:left-auto dark:right-1 transition-all opacity-0 dark:opacity-100" src={moon} alt=""/>
+              <img className="absolute top-1/2 -translate-y-1/2 left-1 dark:left-auto dark:right-1 transition-all opacity-100 dark:opacity-0" src={circle} alt=""/>
+          </span>
+
+          <div className="spacerLine bg-black dark:bg-white h-px"/>
 
           <div className="brand">
-              <img src={logo} alt=""/>
+              { colorTheme === 'light' ? (
+                  <img src={logoDarkMode} alt=""/>
+              ): (
+                  <img src={logo} alt=""/>
+              )}
           </div>
 
-          <div className="menu-trigger relative h-10 w-10 m-2">
+          <div className="spacerLine bg-black dark:bg-white h-px"/>
+
+          <div className={"menu-trigger relative h-10 w-10 m-2"}>
               <span className="
-              line w-10 bg-black h-1 absolute top-1/2 m-auto
+              line w-10 bg-black h-1 absolute top-1/2 m-auto dark:bg-white
 
-              before:w-4 before:absolute before:bg-black before:h-1 before:-top-2
+              before:w-4 before:absolute before:bg-black before:h-1 before:-top-3 before:dark:bg-white
 
-              after:w-4 after:absolute after:bg-black after:h-1 after:top-2 after:right-0
+              after:w-4 after:absolute after:bg-black after:h-1 after:top-3 after:right-0 after:dark:bg-white
               " />
           </div>
       </nav>
